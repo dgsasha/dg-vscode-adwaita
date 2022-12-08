2f37ab7ef89784ba85ed479daacff8a49a65e8e7
dgmarie <0001dgmarie@gmail.com>
2022-12-08 10:10:22 -0500

    Add support for flatpak vscode

--- install.py
+++ install.py
@@ -12,10 +12,16 @@ BGREEN = '\033[1;32m'
 USER = environ.get('USER')
 HOME = path.expanduser(f'~{USER}')
 REPO_DIR = path.dirname(path.realpath(__file__))
-USER_CONFIG_DIR = f'{HOME}/.config/Code/User'
-CODE_CONFIG = f'{USER_CONFIG_DIR}/settings.json'
-BACKUP_CONFIG = f'{USER_CONFIG_DIR}/settings.json.bak'
-EXTENSION_DIR = f'{HOME}/.vscode/extensions/qualia'
+
+USER_CONFIG_DIRS = [
+    f'{HOME}/.config/Code/User',
+    f'{HOME}/.var/app/com.visualstudio.code/config/Code/User'
+]
+
+EXTENSIONS_DIRS = [
+    f'{HOME}/.vscode/extensions',
+    f'{HOME}/.var/app/com.visualstudio.code/data/vscode/extensions'
+]
 
 ACCENTS = (
     'orange',
@@ -31,9 +37,6 @@ ACCENTS = (
     'pink',
     'red'
 )
-
-makedirs(EXTENSION_DIR, exist_ok=True)
-makedirs(USER_CONFIG_DIR, exist_ok=True)
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -63,52 +66,64 @@ theme = args.t
 accent_name = ' ' + accent if accent != 'orange' else ''
 default_syntax = ' & default syntax highlighting' if args.d else ''
 
-css_file = f'file:///{EXTENSION_DIR}/css/qualia-{accent}-{theme}.css'
-settings_json = [
-    '{',
-    f'    "workbench.preferredDarkColorTheme": "qualia{accent_name} dark{default_syntax}",',
-    f'    "workbench.preferredLightColorTheme": "qualia{accent_name} light{default_syntax}",',
-    f'    "workbench.colorTheme": "qualia{accent_name} {theme}{default_syntax}",',
-    '    "window.titleBarStyle": "native",',
-    '    "window.menuBarVisibility": "toggle", // Menu bar will be hidden until you press Alt',
-    '    "window.autoDetectColorScheme": true,',
-    '    "window.title": "${rootPath}${separator}Code",',
-    '    "breadcrumbs.enabled": false,',
-    '    "editor.renderLineHighlight": "none",',
-    '    "workbench.iconTheme": null,',
-    '    "workbench.tree.indent": 12,',
-    '}'
-]
+for user_config_dir, extensions_dir in zip(USER_CONFIG_DIRS, EXTENSIONS_DIRS):
+    if not (path.isdir(user_config_dir) and path.isdir(extensions_dir)):
+        continue
 
-print(f'{BGREEN}Installing{NC} the{BOLD} qualia VS Code theme {NC} in {BOLD}{EXTENSION_DIR}{NC}')
-copytree(REPO_DIR, EXTENSION_DIR, ignore=ignore_patterns('.*'), dirs_exist_ok=True)
-        
-output = []
+    code_config = f'{user_config_dir}/settings.json'
+    backup_config = f'{user_config_dir}/settings.json.bak'
 
-if not Path(BACKUP_CONFIG).is_file() and Path(CODE_CONFIG).is_file():
-    copyfile(CODE_CONFIG, BACKUP_CONFIG)
+    extension_dir = f'{extensions_dir}/qualia'
 
-if Path(CODE_CONFIG).is_file():
-    for line in settings_json:
-        dont_write = False
-        r = open(CODE_CONFIG, "r")
-        for l in r:
-            if not l.strip().startswith('"workbench.colorTheme') \
-            and not l.strip().startswith('"workbench.preferredLightColorTheme') \
-            and not l.strip().startswith('"workbench.preferredDarkColorTheme') \
-            and l.strip().startswith(line.strip().split(':')[0]):
-                output.append(l)
-                dont_write = True
-        if not dont_write:
+    makedirs(extension_dir, exist_ok=True)
+
+    css_file = f'file:///{extension_dir}/css/qualia-{accent}-{theme}.css'
+    settings_json = [
+        '{',
+        f'    "workbench.preferredDarkColorTheme": "qualia{accent_name} dark{default_syntax}",',
+        f'    "workbench.preferredLightColorTheme": "qualia{accent_name} light{default_syntax}",',
+        f'    "workbench.colorTheme": "qualia{accent_name} {theme}{default_syntax}",',
+        '    "window.titleBarStyle": "native",',
+        '    "window.menuBarVisibility": "toggle", // Menu bar will be hidden until you press Alt',
+        '    "window.autoDetectColorScheme": true,',
+        '    "window.title": "${rootPath}${separator}Code",',
+        '    "breadcrumbs.enabled": false,',
+        '    "editor.renderLineHighlight": "none",',
+        '    "workbench.iconTheme": null,',
+        '    "workbench.tree.indent": 12,',
+        '}'
+    ]
+
+    print(f'{BGREEN}Installing{NC} the{BOLD} qualia VS Code theme {NC} in {BOLD}{extension_dir}{NC}')
+    copytree(REPO_DIR, extension_dir, ignore=ignore_patterns('.*'), dirs_exist_ok=True)
+
+    output = []
+
+    if not Path(backup_config).is_file() and Path(code_config).is_file():
+        copyfile(code_config, backup_config)
+
+    if Path(code_config).is_file():
+        for line in settings_json:
+            dont_write = False
+            r = open(code_config, "r", encoding='UTF-8')
+            for l in r:
+                if not l.strip().startswith('"workbench.colorTheme') \
+                and not l.strip().startswith('"workbench.preferredLightColorTheme') \
+                and not l.strip().startswith('"workbench.preferredDarkColorTheme') \
+                and l.strip().startswith(line.strip().split(':')[0]):
+                    output.append(l)
+                    dont_write = True
+            if not dont_write:
+                output.append(line + '\n')
+            r.close()
+    else:
+        for line in settings_json:
             output.append(line + '\n')
-        r.close()
-else:
-    for line in settings_json:
-        output.append(line + '\n')
 
-print(f'Writing VS Code confgiguration to {CODE_CONFIG}.')
-print(f"In VS Code, the menu bar will be hidden until you press 'alt'.")
-print(f"If you want to change this behavior, or any other settings, edit the 'settings.json' file.")
-f = open(CODE_CONFIG, "w+")
-f.writelines(output)
-f.close()
+    print(f'Writing VS Code configuration to {code_config}.')
+    f = open(code_config, "w+", encoding='UTF-8')
+    f.writelines(output)
+    f.close()
+
+print("In VS Code, the menu bar will be hidden until you press 'alt'.")
+print("If you want to change this behavior, or any other settings, edit the 'settings.json' file.")
